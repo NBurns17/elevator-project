ec662d703144fd73e7be59332d175bef0f445cd7
NBurns17 <43864405+NBurns17@users.noreply.github.com>
2025-11-29 16:48:57 -0700

    Fixing packaging, execution now starts react server

--- elevator-frontend/main.js
+++ elevator-frontend/main.js
@@ -1,7 +1,13 @@
 import { app, BrowserWindow } from 'electron';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import isDev from 'electron-is-dev';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+let win;
 function createWindow () {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -9,7 +15,24 @@ function createWindow () {
     }
   })
 
-  win.loadURL('http://localhost:5173')
+  let startURL;
+  if(isDev) {
+    startURL = 'http://localhost:5173';
+  } else {
+    startURL = `file://${join(__dirname, 'dist', 'index.html')}`;
+  }
+  win.loadURL(startURL);
+  win.on('closed', () => (win = null));
 }
 
 app.whenReady().then(createWindow)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if(win === null) {
+    createWindow();
+  }
+});

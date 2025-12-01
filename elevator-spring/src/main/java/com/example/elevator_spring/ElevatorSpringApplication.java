7d3a362117c37d77ad9cb32c074590987011203a
NBurns17 <43864405+NBurns17@users.noreply.github.com>
2025-11-30 21:45:24 -0700

    Adding new spring project

---
+++ elevator-spring/src/main/java/com/example/elevator_spring/ElevatorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.elevator_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorSpringApplication.class, args);
+	}
+
+}
